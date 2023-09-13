fbf7baf0339f6c69b3e1252c2d1f5186545aac80
Riccardo Di Bari <riccardodibari@hotmail.it>
2023-09-13 17:33:19 +0200

    primo const

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const variabile = document.getElementById('new-contact') 
+const variabile = document.getElementById('newList') 
 
 variabile.addEventListener('submit', function (e) {
   e.preventDefault()
-  const rigaUno = document.getElementById("newlist'")
+  const rigaUno = document.getElementById("nuova")
   const rigaDue = document.getElementById("first")
   const rigaTre = document.getElementById("second")
   const rigaQuattro =document.getElementById("third")
 
   const newVariabile = {
-    newlist : rigaUno.value,
+    nuova : rigaUno.value,
     first : rigaDue.value,
     second : rigaTre.value,
     third :rigaQuattro.value
@@ -18,13 +18,13 @@ console.log("newvariabile",newVariabile)
  const newContactCard = document.createElement('div') // <div></div>
   newContactCard.classList.add('contact-card')
   newContactCard.innerHTML = `
-    <p>${newVariabile.newlist} 
+    <p>${newVariabile.nuova} 
    <p>${newVariabile.first}</p>
     <p>${newVariabile.second}</p>
     <p>${newVariabile.third}</p>
     <button onclick="deleteCard(event)">ELIMINA</button>`
      
-    newlist.value = ''
+    nuova.value = ''
     first.value = ''
     second.value = ''
     third.value = ''
